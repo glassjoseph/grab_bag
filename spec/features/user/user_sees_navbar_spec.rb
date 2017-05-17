4c16755903df1313e30490b971457118233c57eb
Sam Landfried <samlandfried@gmail.com>
2017-05-17 16:33:45 -0600

    Test 'Public Folders' button in navbar

--- spec/features/user/user_sees_navbar_spec.rb
+++ spec/features/user/user_sees_navbar_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'User can see a navbar' do
   context 'A user is logged in' do
-    scenario 'A logged in user on root path' do
-      user = create :user
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    attr_reader :user
 
+    before do
+      @user = create :user
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
+
+    scenario 'Navbar from root path' do
       visit root_path
 
       within '.navbar' do
@@ -17,6 +21,22 @@ RSpec.feature 'User can see a navbar' do
           expect(page).to have_link 'Logout'
         end
       end
+    end
+
+    scenario 'Public folders button' do
+      folder1 = create :folder, status: :public
+      folder2 = create :folder, status: :public
+      folder3 = create :folder
+
+      visit root_path
+
+      click_on 'Public Folders'
+
+      expect(current_path).to eq folders_path
+
+      expect(page).to have_link folder1.name
+      expect(page).to have_link folder2.name
+      expect(page).to_not have_link folder3.name
     end
   end
 end
