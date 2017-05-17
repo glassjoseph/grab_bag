6964e5713d4ca3d51de0071369f3e30b1aca9348
dannyradden <dradden@gmail.com>
2017-05-17 11:41:48 -0600

    Initial commit for branch [#145558353]

---
+++ spec/features/visitors/visitor_logs_in_spec.rb
@@ -0,0 +1,9 @@
+feature 'Visitor logs in' do
+  context 'Visitor already has an account' do
+    
+# When I visit '/' path
+# And I click on the login button
+# I expect the current path to '/:username/home'
+# I expect to see a flash message "Successfully logged in"
+  end
+end
