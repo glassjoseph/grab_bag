ba9d3a4290ae78e2e1d1130af6d3cde6705ee69e
glassjoseph <glassjoseph@gmail.com>
2017-05-16 18:17:40 -0600

    initial setup

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,26 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean_with(:truncation)
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
