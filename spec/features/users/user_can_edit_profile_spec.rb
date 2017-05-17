d906674f0a736a6e5ddd386292844e2186e7b1f3
glassjoseph <glassjoseph@gmail.com>
2017-05-17 17:36:13 -0600

    test user can edit profile info

---
+++ spec/features/users/user_can_edit_profile_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.feature "User edits their profile page" do
+  context "as a logged in user" do
+    it "user can change their profile info" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit "#{user.nickname}/dashboard"
+      click_on "Edit Profile"
+
+
+      expect(page).to have_selector("input", value: user.name)
+      expect(page).to have_selector("input", value: user.phone)
+      expect(page).to have_selector("input", value: user.email)
+
+      expect(page).to_not have_selector("input", value: user.username)
+
+      fill_in "user[name]", with: "Confucius"
+      fill_in "user[phone]", with: "9999999999"
+      fill_in "user[email]", with: "c@c.com"
+
+      click_on "Update Profile"
+
+      expect(current_path).to eq("#{user.username}/dashboard")
+      expect(page).to have_content("Name: Confucius")
+      expect(page).to have_content("Username: #{user.username}")
+      expect(page).to have_content("Phone Number: 9999999999")
+      expect(page).to have_content("Email: c@c.com")
+    end
+  end
+end
