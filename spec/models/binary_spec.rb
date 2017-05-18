58dd4b8cff3c2d20d5fab9ed452d21f2ab7fd2aa
Sam Landfried <samlandfried@gmail.com>
2017-05-18 09:16:55 -0600

    Add content_type to Binary model

--- spec/models/binary_spec.rb
+++ spec/models/binary_spec.rb
@@ -14,6 +14,12 @@ RSpec.describe Binary do
       expect(file).to_not be_valid
     end
 
+    it 'is invalid without a content type' do
+      file = Binary.new(content_type: '.txt', folder_id: 1, data: text)
+
+      expect(file).to_not be_valid
+    end
+
     it 'is invalid without a folder' do
       file = Binary.new(name: 'sam.pdf', data: text)
 
@@ -27,13 +33,14 @@ RSpec.describe Binary do
     end
 
     it 'is valid with a name, folder and data' do
-      file = Binary.new(name: 'sam.pdf', folder_id: 1, data: text)
+      file = Binary.new(name: 'sam.pdf', content_type: '.pdf', folder_id: 1, data: text)
 
       expect(file).to be_valid
     end
 
-    it 'responds to name, folder and data' do
+    it 'responds to name, content_type, folder and data' do
       expect(file).to respond_to :name
+      expect(file).to respond_to :content_type
       expect(file).to respond_to :folder
       expect(file).to respond_to :data
     end
