403bb36d3567749abc5db9d6cff03c67bcb938fb
bschwartz10 <bschwartz92@gmail.com>
2017-05-17 20:00:32 -0600

    create user model test
    add validations for all user attributes
    create enum for active/inactive
    add shoulda matchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :name, presence: true
+  validates :fb_id, presence: true
+  validates :status, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :token, presence: true
+  validates :avatar_url, presence: true
+  validates_uniqueness_of :username, case_sensitive: false
+
+  enum status: %w(active inactive)
+
   def self.from_omniauth(auth)
     new do |user|
       user.fb_id = auth["uid"]
