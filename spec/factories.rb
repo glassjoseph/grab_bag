a656f921cee8c7373d8d1c7807abbd5c86e3b92e
Sam Landfried <samlandfried@gmail.com>
2017-05-17 18:24:45 -0600

    Add user factory

---
+++ spec/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :user do
+    sequence :name do |n|
+      "Name ##{n}"
+    end
+    sequence :username do |n|
+      "username#{n}"
+    end
+    sequence :fb_id do |n|
+      n.to_s
+    end
+    status 'enabled'
+    sequence :email do |n|
+      "email#{n}@email.com"
+    end
+    phone '5555555555'
+    token ENV['facebook_token']
+    avatar_url 'https://socwork.wisc.edu/files/joe-glass-lg.jpg'
+  end
+end
