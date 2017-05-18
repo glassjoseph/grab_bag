a1aeeb553b7bcf56e3405766260697d67ddc8585
jk1dd <portajonk@gmail.com>
2017-05-18 10:25:33 -0600

    add test for user visiting a file show page

---
+++ spec/features/users/user_visits_file_show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'user visits file show page' do
+  context 'with access to a folder with a file in it' do
+    scenario 'she can click on the file and see things' do
+      user = create(:user)
+
+      stub_oauth
+
+      folder = create(:folder)
+      file = create(:binary)
+      folder.files << file
+      user.folders << folder
+
+      visit "/#{user.username}/#{folder.name}"
+
+      click_on "#{file.name}"
+
+      expect(current_path).to eq("/#{user.username}/#{folder.name}/#{file.name}")
+      expect(page).to have_button("Download")
+      expect(page).to have_field("Comments")
+      expect(page).to have_button("Post Comment")
+    end
+  end
+end
---
+++ spec/features/visitor/visitor_visits_fils_show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature 'Visitor vists a file show page' do
+  context 'a visitor who is not logged in' do
+    scenario 'gets a 404 for a private file' do
+      
+    end
+  end
+end
