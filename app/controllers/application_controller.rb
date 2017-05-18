3c64d030a78012f11c955d5a29ffbb4a08bb3b4f
Sam Landfried <samlandfried@gmail.com>
2017-05-17 18:30:11 -0600

    Add current_user to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+  helper_method :current_user
+
+  def current_user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
