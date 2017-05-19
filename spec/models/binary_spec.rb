d2494988878641627735e6dbbac61f3666505703
Sam Landfried <samlandfried@gmail.com>
2017-05-19 17:16:13 -0600

    Change content_type to 'extension'

--- spec/models/binary_spec.rb
+++ spec/models/binary_spec.rb
@@ -9,44 +9,48 @@ RSpec.describe Binary do
 
   context 'Attributes' do
     it 'is invalid without a name' do
-      file = Binary.new(folder_id: 1, data: text)
+      file = Binary.new(folder_id: 1, data: text, extention: 'txt')
 
       expect(file).to_not be_valid
     end
 
-    it 'is invalid without a content type' do
-      file = Binary.new(content_type: '.txt', folder_id: 1, data: text)
+    it 'is invalid without an extension' do
+      file = Binary.new(name: 'test', folder_id: 1, data: text)
 
       expect(file).to_not be_valid
     end
 
     it 'is invalid without a folder' do
-      file = Binary.new(name: 'sam.pdf', data: text)
+      file = Binary.new(name: 'sam.pdf', data: text, extention: 'txt')
 
       expect(file).to_not be_valid
     end
 
     it 'is invalid without data' do
-      file = Binary.new(name: 'sam.pdf', folder_id: 1)
+      file = Binary.new(name: 'sam.pdf', folder_id: 1, extention: 'txt')
 
       expect(file).to_not be_valid
     end
 
-    it 'is valid with a name, folder and data' do
+    it 'is valid with a name, folder, data and extension' do
       user = create :user
       folder = user.home
-      file = Binary.new(name: 'sam.pdf', content_type: '.pdf', folder: folder, data: text)
+      file = Binary.new(name: 'sam.pdf',
+                        extension: '.pdf',
+                        folder: folder,
+                        data: text)
 
       expect(file).to be_valid
     end
 
-    it 'responds to name, content_type, folder and data' do
+    it 'responds to name, extension, folder and data' do
       file = Binary.new
 
       expect(file).to respond_to :name
-      expect(file).to respond_to :content_type
+      expect(file).to respond_to :extension
       expect(file).to respond_to :folder
       expect(file).to respond_to :data
+      expect(file).to respond_to :url
     end
   end
 
@@ -56,6 +60,8 @@ RSpec.describe Binary do
       folder = user.home
 
       file = Binary.new(name: 'sam.pdf', folder: folder, data: text)
+
+      expect(file.folder).to be folder
     end
   end
 
@@ -65,7 +71,9 @@ RSpec.describe Binary do
       folder = user.home
       binary = create :binary, folder: folder
 
-      expect(binary.url).to eq '/' + user.username + '/' + folder.route
+      path = folder.url + '/' + binary.name + '.' + binary.extension
+
+      expect(binary.url).to eq path
     end
   end
 end
