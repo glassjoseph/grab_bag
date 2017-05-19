a9df51c4cb8b163a9462ea2d6de5eecdb0bd5219
dannyradden <dradden@gmail.com>
2017-05-19 15:36:53 -0600

    Auto-generate route of folders
    Organize folder controller
    Remove route from factory

--- app/controllers/users/folders_controller.rb
+++ app/controllers/users/folders_controller.rb
@@ -1,12 +1,9 @@
 class Users::FoldersController < ApplicationController
 
   def show
-    current_url = request.env["PATH_INFO"]
-    username = current_url.split('/')[1]
-    folder_route = current_url.split('/')[2..-1].join('/')
-    user = User.find_by(username: username)
+    user = User.find_by(username: params[:username])
 
-    if @current_folder = Folder.find_by(route: folder_route, user_id: user.id)
+    if @current_folder = Folder.find_by(route: params[:route], user_id: user.id)
       session[:folder_id] = @current_folder.id
     else
       render :file => "#{Rails.root}/public/404.html",  :status => 404
@@ -23,18 +20,14 @@ class Users::FoldersController < ApplicationController
   def create
     parent_folder = Folder.find(session[:folder_id])
     session.delete(:folder_id)
-    new_route = parent_folder.route + "/" + folder_params[:name].parameterize
 
     folder = Folder.new(folder_params)
     if folder.update(user_id: parent_folder.owner.id,
-                     route: new_route,
                      folder_id: parent_folder.id)
-
       redirect_to folder.url, success: "Folder Successfully Created!"
     else
       redirect_to parent_folder.url, warning: "Folder already exists."
     end
-
   end
 
 
