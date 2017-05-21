ed644554056f7de4f098112d24da575631748faf
Sam Landfried <samlandfried@gmail.com>
2017-05-21 12:12:27 -0600

    Alter Binary model for #data_url

--- app/models/binary.rb
+++ app/models/binary.rb
@@ -1,28 +1,29 @@
 class Binary < ApplicationRecord
   validates :name, presence: true
   validates :extension, presence: true
-  validates :data, presence: true
+  validates :data_url, presence: true
 
   belongs_to :folder
-
-  def uploaded_file=(incoming_file)
-    self.name = incoming_file.original_filename
-    self.extension = incoming_file.extension
-    self.data = incoming_file.read
-  end
-
-  def name=(new_filename)
-    write_attribute("name", sanitize_filename(new_filename))
-  end
 
   def url
     folder.url + '/' + name + '.' + extension
   end
 
-private
+#   I believe this will be unnecessary for an S3 implementation
+#
+#   def uploaded_file=(incoming_file)
+#     self.name = incoming_file.original_filename
+#     self.extension = incoming_file.extension
+#     self.data = incoming_file.read
+#   end
 
-  def sanitize_filename(filename)
-    just_filename = File.basename(filename)
-    just_filename.gsub(/[^\w\.\-]/, '_')
-  end
+#   def name=(new_filename)
+#     write_attribute("name", sanitize_filename(new_filename))
+#   end
+# private
+
+#   def sanitize_filename(filename)
+#     just_filename = File.basename(filename)
+#     just_filename.gsub(/[^\w\.\-]/, '_')
+#   end
 end
