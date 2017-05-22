e1e634fb8155a91ff537e14a855dde3a5a00470e
bschwartz10 <bschwartz92@gmail.com>
2017-05-21 18:34:57 -0600

    WIP update binaries controller to include amazon s3
    set a before action for new and create actions
    create binary params to take in data_url
    update binary model to set name, extension and folder_id fields
    NOT WORKING redirecting to file show page

--- app/controllers/users/folders/binaries_controller.rb
+++ app/controllers/users/folders/binaries_controller.rb
@@ -1,20 +1,22 @@
 class Users::Folders::BinariesController < ApplicationController
+  before_action :set_s3_direct_post, only: [:new, :create]
+
   def new
+    @binary = Binary.new
   end
 
   def create
-    return if params[:upload_file].blank?
     user = User.find_by(username: params[:username])
     folder = user.owned_folders.find_by(route: params[:route])
-    @binary = folder.binaries.new
-
-    # @binary.uploaded_file = params[:upload_file]
-    #
-    # if @binary.save!
-    #   redirect_to landing_page_path, success: "Profile Updated"
-    # else
-    #   render :new, error: "There was a problem submitting your attachment."
-    # end
+    binary = Binary.new(binary_params)
+    if binary.update(name: params["binary"]["data_url"].original_filename.split(".")[0],
+                     extension: params["binary"]["data_url"].original_filename.split(".")[1],
+                     folder_id: folder.id
+      )
+      redirect_to binary.url, success: "File created"
+    else
+      render :new, error: "There was a problem submitting your attachment."
+    end
   end
 
   def show
@@ -22,7 +24,17 @@ class Users::Folders::BinariesController < ApplicationController
     folder = user.owned_folders.find_by(route: params[:route])
 
     @binary = Binary.find_by(name: params[:binary_name])
-
     render 'users/folders/binaries/show.html.erb'
   end
+
+  private
+
+  def binary_params
+    params.require(:binary).permit(:data_url)
+  end
+
+  def set_s3_direct_post
+   @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
+
 end
