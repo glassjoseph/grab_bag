d11a32402bd5a887ce9dfe15cd9b3df8df75f336
bschwartz10 <bschwartz92@gmail.com>
2017-05-22 15:12:40 -0600

    test for deleting a binary

---
+++ spec/features/users/user_can_delete_a_binary_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature do
+  context "as a logged in user" do
+    it "can delete my own file" do
+      user = create(:user_with_folders)
+      binary = user.home.binaries.first
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit "/#{user.username}/home"
+      first('.binaries > tr').click_on "Delete"
+
+      expect(page).to have_content("#{binary.name} Successfully Deleted")
+      expect(current_path).to eq("/#{user.username}/home")
+    end
+  end
+end
