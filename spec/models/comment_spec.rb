f57c05e164e3c60de1a02f0430e7bf48f7490b6e
jk1dd <portajonk@gmail.com>
2017-05-22 17:32:13 -0600

    update comment spec test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,36 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
-  attr_reader :comment
-
-  before do
-    @comment = Comment.new(text: 'This comment adds to the civil discussion of some topic LOL.')
-  end
 
   context 'Attributes' do
     it 'is invalid without text' do
-      comment = Comment.new()
+      binary = create(:binary)
+      comment = Comment.new(binary: binary)
 
       expect(comment).to_not be_valid
     end
 
-    it 'is valid with a text' do
+    it 'is invalid without binary' do
+      comment = Comment.new(text: 'This comment adds to the civil discussion of some topic LOL.')
+
+      expect(comment).to_not be_valid
+    end
+
+    it 'is valid with a text and binary' do
+      binary = create(:binary)
+      comment = Comment.new(text: 'This comment adds to the civil discussion of some topic LOL.', binary: binary)
+
       expect(comment).to be_valid
     end
 
-    it 'responds to text' do
-      expect(comment).to respond_to :text
+    it 'responds to text and binary' do
+      expect(comment).to respond_to(:text)
+      expect(comment).to respond_to(:binary)
     end
   end
 
   context 'relationships' do
-    it 'has a binary' do
+    it 'belongs_to a binary' do
       user = create(:user)
-      folder = user.home
-      file = create(:text_binary)
+      user.home.binaries << create(:binary, folder: user.home)
       comment = create(:comment)
-      file.comments << comment
-      folder.binaries << file
+      user.home.binaries.first.comments << comment
+
+      expect(user.home.binaries.first.comments.first).to eq(comment)
+      expect(user.home.binaries.first.comments.first).to be_a(Comment)
     end
   end
 end
