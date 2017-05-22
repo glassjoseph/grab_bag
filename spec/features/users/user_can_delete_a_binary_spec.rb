c81592a68d6256066485ef650cdad38843885daf
bschwartz10 <bschwartz92@gmail.com>
2017-05-22 15:22:30 -0600

    user can delete a binary from the binary show page

--- spec/features/users/user_can_delete_a_binary_spec.rb
+++ spec/features/users/user_can_delete_a_binary_spec.rb
@@ -2,13 +2,24 @@ require 'rails_helper'
 
 RSpec.feature do
   context "as a logged in user" do
-    it "can delete my own file from a folder show page" do
+    it "can delete my own binary from a folder show page" do
       user = create(:user_with_folders)
       binary = user.home.binaries.first
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit "/#{user.username}/home"
       first('.binaries > tr').click_on "Delete"
+
+      expect(page).to have_content("#{binary.name} Successfully Deleted")
+      expect(current_path).to eq("/#{user.username}/home")
+    end
+    it "can delete my own binary from a binary show page" do
+      user = create(:user_with_folders)
+      binary = user.home.binaries.first
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit binary.url
+
+      click_on "Delete"
 
       expect(page).to have_content("#{binary.name} Successfully Deleted")
       expect(current_path).to eq("/#{user.username}/home")
