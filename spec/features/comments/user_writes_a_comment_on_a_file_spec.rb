68fc03fb4d35f369d6d7c73277c521487e92fb13
jk1dd <portajonk@gmail.com>
2017-05-22 17:14:48 -0600

    update user comment test

--- spec/features/comments/user_writes_a_comment_on_a_file_spec.rb
+++ spec/features/comments/user_writes_a_comment_on_a_file_spec.rb
@@ -1,33 +1,49 @@
-# As a user
-# With access to a file
-# And I go to a file's show page
-# And I type a message in the comment textarea
-# And I click 'Post Comment'
-# Then I expect the current path to be the file's show page
-# And I expect the comment to be posted on the page
-
 require 'rails_helper'
 
 RSpec.feature 'A user can leave a comment on a file' do
   context 'when it is her own file' do
     scenario 'she can comment on it' do
-      user = create(:user_with_folders)
-      binding.pry
+      user = create(:user)
+      folder = user.home
+      binary = create(:binary, folder: folder)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      folder = user.home.folders.first
 
       visit folder.url
 
-      click_on "#{text.name}"
+      click_on binary.name
 
-      expect(current_path).to eq("/#{user.username}/home/#{folder.route}/#{text.name}")
+      expect(current_path).to eq(binary.url)
       expect(page).to have_field('Comment')
 
       fill_in "Comment", with: "This is a comment."
       click_on('Post Comment')
 
-      expect(current_path).to eq("/#{user.username}/home/#{folder.route}/#{text.name}")
+      expect(current_path).to eq(binary.url)
+      expect(page).to have_content('This is a comment.')
+    end
+  end
+
+  context 'when it is a public file' do
+    scenario 'she can comment on it' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      folder = create(:folder, name: "keyboard", parent: user.home, permission: "global")
+      folder2 = create(:folder, name: "phone", parent: folder)
+      binary = create(:binary, name: "mouse", folder: folder)
+
+      visit folder.url
+
+      expect(current_path).to eq(folder.url)
+      click_on binary.name
+
+      expect(current_path).to eq(binary.name)
+      expect(page).to have_field('Comment')
+
+      fill_in "Comment", with: 'This is a comment.'
+      click_on('Post Comment')
+
+      expect(current_path).to eq(binary.url)
       expect(page).to have_content('This is a comment.')
     end
   end
@@ -61,33 +77,4 @@ RSpec.feature 'A user can leave a comment on a file' do
   #   end
   # end
   #
-  # context 'when it is a public file' do
-  #   scenario 'she can comment on it' do
-  #     user1, user2 = create_list(:user)
-  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #
-  #     folder = create(:folder)
-  #     text = create(:text_binary)
-  #     folder.binaries << text
-  #     user.home.folders << folder
-  #
-  #     visit "/#{user1.username}/home/#{folder.route}"
-  #     click_on 'Share'
-  #     choose('Public')
-  #     click_on('Save')
-  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  #
-  #     visit "/#{user1.username}/home/#{folder.route}"
-  #     click_on "#{text.name}"
-  #
-  #     expect(current_path).to eq("/#{user1.username}/home/#{folder.route}/#{text.name}")
-  #     expect(page).to have_field('Comment')
-  #
-  #     fill_in('Comment'), with: 'This is a comment.'
-  #     click_on('Post Comment')
-  #
-  #     expect(current_path).to eq("/#{user1.username}/home/#{folder.route}/#{text.name}")
-  #     expect(page).to have_content('This is a comment.')
-  #   end
-  # end
 end
