88472ceb65318b1e6dc1da1823e076c69472f521
Sam Landfried <samlandfried@gmail.com>
2017-05-23 09:21:48 -0600

    Touch up indenting

--- spec/mock_auth_helper.rb
+++ spec/mock_auth_helper.rb
@@ -1,4 +1,3 @@
-
 RSpec.configure do
   def stub_oauth
     OmniAuth.config.test_mode = true
@@ -6,16 +5,23 @@ RSpec.configure do
     auth_hash = {
       "provider"=>"facebook",
       "uid"=>"10103559484486366",
-      "info"=>
-      {"email"=>ENV['facebook_email'],
-       "name"=>ENV['facebook_name'],
-       "image"=>"http://graph.facebook.com/v2.6/10103559484486366/picture"},
-      "credentials"=>
-      {"token"=> ENV['facebook_token'],
-       "expires_at"=>1500238611,
-
-       "expires"=>true},
-      "extra"=>{"raw_info"=>{"name"=>ENV['facebook_name'], "email"=>ENV['facebook_email'], "id"=>"10103559484486366"}}
+      "info"=> {
+        "email"=>ENV['facebook_email'],
+        "name"=>ENV['facebook_name'],
+        "image"=>"http://graph.facebook.com/v2.6/10103559484486366/picture"
+      },
+      "credentials"=> {
+        "token"=> ENV['facebook_token'],
+        "expires_at"=>1500238611,
+        "expires"=>true
+      },
+      "extra"=> {
+        "raw_info"=> {
+          "name"=>ENV['facebook_name'],
+          "email"=>ENV['facebook_email'],
+          "id"=>"10103559484486366"
+        }
+      }
     }
 
     OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(auth_hash)
