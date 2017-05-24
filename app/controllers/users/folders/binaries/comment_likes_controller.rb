738ac33c01008e8ceb6bd3afa1bd6250516efecb
jk1dd <portajonk@gmail.com>
2017-05-24 11:59:10 -0600

    fix comment like controller to find proper comment

--- app/controllers/users/folders/binaries/comment_likes_controller.rb
+++ app/controllers/users/folders/binaries/comment_likes_controller.rb
@@ -2,7 +2,7 @@ class Users::Folders::Binaries::CommentLikesController < ApplicationController
   def create
     binary = Binary.find_by(name: params[:binary_name])
     user = binary.folder.owner
-    comment = binary.comments.first
+    comment = binary.comments.find_by(binary_id: binary)
     like = comment.likes.new(user: user)
     like.update(like: true)
     redirect_to(binary.url)
