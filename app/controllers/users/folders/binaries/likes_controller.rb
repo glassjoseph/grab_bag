ae4f9e12b88ba4b338c5e51328a729b9519665eb
jk1dd <portajonk@gmail.com>
2017-05-24 10:39:53 -0600

    route, though incorrect, hits the right controller and like can be created

--- app/controllers/users/folders/binaries/likes_controller.rb
+++ app/controllers/users/folders/binaries/likes_controller.rb
@@ -1,5 +1,10 @@
 class Users::Folders::Binaries::LikesController < ApplicationController
   def create
-    
+    binary = Binary.find_by(name: params[:binary_name])
+    user = binary.folder.owner
+    like = binary.likes.new(user: user)
+    like.update(like: true)
+    # binding.pry
+    redirect_to binary.url
   end
 end
