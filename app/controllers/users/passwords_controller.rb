a34dc2c27927ecd1776202a5d84587da38fc6579
bschwartz10 <bschwartz92@gmail.com>
2017-05-24 11:37:08 -0600

    update passwords controller with update and custom methods
    add update action to authenticated before action
    create update action to update a user password if successful
    create password_not_empty and password_valid filter methods

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,5 @@
 class Users::PasswordsController < ApplicationController
-  before_action :authenticated?, only: [:edit]
+  before_action :authenticated?, only: [:edit, :update]
 
   def edit
     @user = current_user
@@ -11,10 +11,32 @@ class Users::PasswordsController < ApplicationController
     redirect_to new_confirmation_path
   end
 
+  def update
+    if password_not_empty? && password_valid?
+      current_user.update(password_params)
+      redirect_to users_dashboard_path(current_user.username), success: "Password Updated"
+      session[:authenticated] = false
+    else
+      redirect_to users_password_edit_path(current_user.username), warning: "Error, please try again."
+    end
+  end
+
   private
 
   def authenticated?
-    render :file => "#{Rails.root}/public/404.html", :status => 404 unless session[:authenticated] = true
+    render :file => "#{Rails.root}/public/404.html", :status => 404 unless session[:authenticated]
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def password_not_empty?
+    params[:user][:password] != nil || params[:user][:password_confirmation] != nil
+  end
+
+  def password_valid?
+    params[:user][:password] == params[:user][:password_confirmation]
   end
 
 end
