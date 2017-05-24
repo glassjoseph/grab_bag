476d221c81ebeddb981a1d1826af23b0e230e3b1
bschwartz10 <bschwartz92@gmail.com>
2017-05-24 17:49:04 -0600

    add default avatar url to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524012307) do
+ActiveRecord::Schema.define(version: 20170524233535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,12 +66,12 @@ ActiveRecord::Schema.define(version: 20170524012307) do
     t.string   "email"
     t.string   "phone"
     t.string   "token"
-    t.string   "avatar_url"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",                                                                    null: false
+    t.datetime "updated_at",                                                                    null: false
     t.string   "password_digest"
-    t.integer  "role",            default: 0
+    t.integer  "role",              default: 0
     t.string   "verification_code"
+    t.string   "avatar_url",        default: "https://thumb.ibb.co/htakav/default_profile.jpg"
   end
 
   add_foreign_key "binaries", "folders"
