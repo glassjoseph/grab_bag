5f753a8d87ff62c7dfbc2c61b8489f58e195cb15
glassjoseph <glassjoseph@gmail.com>
2017-05-24 20:27:26 -0600

    Finish admin_can_delete_spec

--- spec/features/admins/admin_can_delete_spec.rb
+++ spec/features/admins/admin_can_delete_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 require 'helpers/admin_helper'
 
-
 RSpec.feature "Admin can delete items" do
-  xcontext "As an Admin" do
+  context "As an Admin" do
     scenario "admin can delete user's folder" do
       stub_admin
       user = create(:user_with_folders)
       expect(user.home.folders.count).to eq(3)
       visit "/#{user.username}/home"
-      save_and_open_page
 
-      expect{ within(".folder") do
+      expect{ within(first(".folder")) do
                 click_on "Delete"
               end
-              click_on "OK"
       }.to change{user.home.folders.count}.from(3).to(2)
 
     expect(current_path).to eq("/#{user.username}/home")
@@ -28,10 +25,9 @@ RSpec.feature "Admin can delete items" do
       expect(user.home.folders.count).to eq(3)
       visit "/#{user.username}/home"
 
-      expect{ within(".binary") do
+      expect{ within(first(".binary")) do
                 click_on "Delete"
               end
-              click_on "OK"
       }.to change{user.home.binaries.count}.from(3).to(2)
 
     expect(current_path).to eq("/#{user.username}/home")
